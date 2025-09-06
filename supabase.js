d007ab074b2effc9698b99cd3f4ba5ab6f1b86ee
Nikhil Sinha <niksy2006@gmail.com>
2025-09-07 04:05:51 +0530

    Added phone number verification to supabase data

--- supabase.js
+++ supabase.js
@@ -2,3 +2,17 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://yloptztnpoexynypgxau.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
+export async function addUserInfo(phone, name, email, address) {
+  const { error } = await supabase.from("user_info").upsert([
+    {
+      id: phone,
+      name,
+      email,
+      phone,
+      address
+    }
+  ]);
+
+  if (error) console.error(error);
+}
